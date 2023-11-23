19c527ddb9905d00be0fd252099d170f7b4f335d
Hala Alsuwayt <halaals>
2023-11-23 14:37:26 +0300

    test git command 2

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,6 +1,7 @@
 import { Category, ICategory } from '../models/category'
 import { createHttpError } from '../util/createHttpError'
 // test 1
+// test 2
 export const findCategoryById = async (id: string): Promise<ICategory> => {
   const category = await Category.findOne({ _id: id })
   if (!category) {
