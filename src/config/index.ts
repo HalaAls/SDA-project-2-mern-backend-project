9f6564c93561481bfaf49cd9a0205d7ba666077d
Amal Gh <Algamdi_8@hotmail.com>
2023-11-26 02:01:04 +0300

    Integrate JWT

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,13 @@
 import 'dotenv/config'
 
 export const dev = {
-  app: { port: Number(process.env.PORT) || 5050 },
+  app: {
+    port: Number(process.env.PORT) || 5050,
+    defaultImagePath: process.env.DEFAULT_IMAGE_PATH || 'public/images/users/default.png',
+    jwtUserActivationKey: process.env.JWT_USER_ACTIVATION_KEY || 'shhhhh',
+    smtpUsername: process.env.SMTP_USERNAME || 'amlalgamdi.80@gmail.com',
+    smtpPassword: process.env.SMTP_PASSWORD || 'noxo hymh pees teql',
+  },
   db: {
     url: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/sda-ecommerce-db',
   },
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,20 +1,26 @@
 import { Router } from 'express'
 
-import { CreateUser, deleteUser, getAllUsers, getSingleUser, registerUser, updateUserByEmail } from '../controllers/userController'
+import {
+  CreateUser,
+  deleteUser,
+  getAllUsers,
+  getSingleUser,
+  processRegisterUser,
+  updateUserByEmail,
+  activateUser,
+} from '../controllers/userController'
 import { uploadImageUser } from '../middlewares/uploadFile'
 
 const router = Router()
 
-router.post('/register', uploadImageUser.single('image'), registerUser)
+router.post('/process-register', uploadImageUser.single('image'), processRegisterUser)
+router.post('/activate', uploadImageUser.single('image'), activateUser)
 router.post('/', uploadImageUser.single('image'), CreateUser)
 
 router.get('/', getAllUsers)
-router.get('/:email',getSingleUser)
+router.get('/:email', getSingleUser)
 
 router.delete('/:email', deleteUser)
 router.put('/:email', uploadImageUser.single('image'), updateUserByEmail)
-
-
-
 
 export default router
